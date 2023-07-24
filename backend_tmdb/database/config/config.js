eeffb5250b5fe57a6dd4d3b8a024938361cb80ac
MLauzan <mateolauzan@gmail.com>
2023-07-23 23:02:52 -0300

    config changes

--- backend_tmdb/database/config/config.js
+++ backend_tmdb/database/config/config.js
@@ -8,20 +8,6 @@ module.exports = {
     "host": process.env.MYSQLHOST,
     "port": process.env.MYSQLPORT,
     "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
   }
 }
 
