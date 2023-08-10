e0ebb0c98cb315a7776b986a1a605f72ca1f7c7b
MateoL1234 <140357158+MateoL1234@users.noreply.github.com>
2023-08-10 17:19:38 -0300

    Update app.js

--- backend_tmdb/app.js
+++ backend_tmdb/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3032
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-const origin = ['https://mlmovieapp.netlify.app', "http://localhost:7000", "https://postman.com"]
+const origin = ['https://mlmovieapp.netlify.app', "http://localhost:3000", "https://postman.com"]
 app.use(cors({ origin: origin, credentials: true }));
 
 const userApiRoutes = require('./router');
